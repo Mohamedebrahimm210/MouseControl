329e8cb14bab96c56382e56277cd84909248388d
Mohamed Ebrahim Abdelslam <67837187+Mohamedebrahimm210@users.noreply.github.com>
2025-05-05 17:26:55 +0300

    First Commit

---
+++ MouseControl/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MouseControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            btnStart = new Button();
+            btnStop = new Button();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            lblTimer = new Label();
+            uiTimer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(48, 91);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(94, 29);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(178, 91);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(94, 29);
+            btnStop.TabIndex = 1;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveBorder;
+            label1.Location = new Point(678, 427);
+            label1.Name = "label1";
+            label1.Size = new Size(120, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Copyright Abosa";
+            // 
+            // lblTimer
+            // 
+            lblTimer.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblTimer.Location = new Point(48, 31);
+            lblTimer.Name = "lblTimer";
+            lblTimer.Size = new Size(224, 30);
+            lblTimer.TabIndex = 4;
+            lblTimer.Text = "Timer: Not started";
+            // 
+            // uiTimer
+            // 
+            uiTimer.Interval = 1000;
+            uiTimer.Tick += UiTimer_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Controls.Add(lblTimer);
+            Name = "Form1";
+            Text = "Mouse Mover";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnStart;
+        private Button btnStop;
+        private Label label1;
+        private Label lblTimer;
+        private System.Windows.Forms.Timer uiTimer; // Timer for countdown display
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ MouseControl/Form1.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseControl
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("kernel32.dll")]
+        static extern uint SetThreadExecutionState(uint esFlags);
+
+        private const uint ES_CONTINUOUS = 0x80000000;
+        private const uint ES_DISPLAY_REQUIRED = 0x00000002;
+
+        private CancellationTokenSource _cts;
+        private Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnStart.Click += BtnStart_Click;
+            btnStop.Click += BtnStop_Click;
+
+            // Try to set a custom icon if available
+            try
+            {
+                if (System.IO.File.Exists("appicon.ico"))
+                    this.Icon = new Icon("appicon.ico");
+            }
+            catch
+            {
+                // Ignore invalid icon
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            countdownSeconds = 120;
+            uiTimer.Start(); // Start UI countdown
+
+            if (_cts != null)
+            {
+                MessageBox.Show("Mouse movement is already running.");
+                return;
+            }
+
+            _cts = new CancellationTokenSource();
+            StartMouseMover(_cts.Token);
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            uiTimer.Stop();
+            lblTimer.Text = "Timer: Stopped";
+
+            if (_cts != null)
+            {
+                _cts.Cancel();
+                _cts = null;
+                MessageBox.Show("Mouse movement stopped.");
+            }
+        }
+        
+        private async void StartMouseMover(CancellationToken token)
+        {
+            MessageBox.Show("Mouse movement started. It will move every 2 minutes.");
+
+            while (!token.IsCancellationRequested)
+            {
+                SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
+
+                int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+                int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+                int x = _random.Next(0, screenWidth);
+                int y = _random.Next(0, screenHeight);
+                SetCursorPos(x, y);
+
+                try
+                {
+                    await Task.Delay(TimeSpan.FromMinutes(2), token);
+                }
+                catch (TaskCanceledException)
+                {
+                    break;
+                }
+            }
+
+            SetThreadExecutionState(ES_CONTINUOUS);
+        }
+
+        private int countdownSeconds = 120; // 2 minutes in seconds
+        private void UiTimer_Tick(object sender, EventArgs e)
+        {
+            countdownSeconds--;
+
+            TimeSpan timeLeft = TimeSpan.FromSeconds(countdownSeconds);
+            lblTimer.Text = $"Next move in: {timeLeft:mm\\:ss}";
+
+            if (countdownSeconds <= 0)
+            {
+                countdownSeconds = 120; // reset
+            }
+        }
+    }
+}
